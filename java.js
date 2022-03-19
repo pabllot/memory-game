c7c1d8a601f696025059340a391ef446342ae8d9
Pablo Teixeira <101361880+pabllot@users.noreply.github.com>
2022-03-18 22:16:27 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,32 @@
+function show(){
+    document.querySelector(".a1").classList.toggle("show")
+} 
+
+function show2(){
+    document.querySelector(".a2").classList.toggle("show")
+} 
+
+function show3(){
+    document.querySelector(".a3").classList.toggle("show")
+} 
+
+function show4(){
+    document.querySelector(".a4").classList.toggle("show")
+} 
+
+function show5(){
+    document.querySelector(".a5").classList.toggle("show")
+} 
+
+function show6(){
+    document.querySelector(".a6").classList.toggle("show")
+} 
+
+function show7(){
+    document.querySelector(".a7").classList.toggle("show")
+} 
+
+function show8(){
+    document.querySelector(".a8").classList.toggle("show")
+} 
+
